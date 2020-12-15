c3085c92375721aef9f3ab4698ceb98ab9e1e1bf
tinhhiepban <truongdat1408@gmail.com>
2020-12-15 13:54:13 +0700

    account, info, support

--- app/src/main/java/com/huy3999/schedules/fragment/SettingsFragment.java
+++ app/src/main/java/com/huy3999/schedules/fragment/SettingsFragment.java
@@ -3,6 +3,7 @@ package com.huy3999.schedules.fragment;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -11,6 +12,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.accounts.Account;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.InflateException;
@@ -28,7 +30,7 @@ import petrov.kristiyan.colorpicker.ColorPicker;
 
 public class SettingsFragment extends Fragment {
     private static View root;
-    private LinearLayout btnAccount, btnColorTheme;
+    private LinearLayout btnAccount, btnColorTheme, btnInfo, btnSupport;
     private String colorChoosed;
     @Nullable
     @Override
@@ -36,6 +38,8 @@ public class SettingsFragment extends Fragment {
         root = inflater.inflate(R.layout.activity_settings_fragment, container, false);
         btnAccount = root.findViewById(R.id.account_settings);
         btnColorTheme = root.findViewById(R.id.color_theme_settings);
+        btnInfo = root.findViewById(R.id.infomation_settings);
+        btnSupport = root.findViewById(R.id.support_settings);
 
         btnAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +49,39 @@ public class SettingsFragment extends Fragment {
                 ft.replace(container.getId(), new AccountFragment()).commit();
             }
         });
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog al = settingsDialog("Infomation","Schedule Application - version3 - Tr.CongDat,Ng.ThiTram,Ph.ThanhHuy,Ng.TienVan");
+                al.show();
+            }
+        });
+
+        btnSupport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog al = settingsDialog("Support","If you need some helps, please call: 1900 6088 (Chi ong vang)");
+                al.show();
+            }
+        });
         return root;
+    }
+
+    private AlertDialog settingsDialog(String title, String s) {
+        //Dialog
+        AlertDialog.Builder b = new AlertDialog.Builder(getContext());
+        //Thiết lập tiêu đề
+        b.setTitle(title);
+        b.setMessage(s);
+        // Nút Ok
+        b.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        return b.create();
     }
 
 
